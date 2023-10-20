de387152e03368e577222126204a9c0b4d51846d
Марина Катаева <marina.k.study@yandex.ru>
2023-10-20 23:13:23 +0500

    New version

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,6 +3,7 @@ import re
 
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
